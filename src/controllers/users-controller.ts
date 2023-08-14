e6912b92cb60d3f7e605d422016d63ebb7b2f3e6
Rlopeze <mumolopeze@uc.cl>
2023-08-14 16:28:52 -0400

    :smile: user controller updates

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,19 +1,36 @@
-// import db from '../models'
-// const bcrypt = require("bcrypt")
-import { RequestHandler } from "express"
+import db from "../models";
+const bcrypt = require("bcrypt")
+import { RequestHandler } from "express";
 
 interface RequestUserBody {
-    name: string,
-    lastName: string,
-    email: string,
-    password: string
+  name: string;
+  lastName: string;
+  email: string;
+  password: string;
 }
 
-const createUser: RequestHandler= async (req , res) =>{
-    const userData = <RequestUserBody> req.body
-    console.log(userData)
-    // const a = await bcrypt.hash(userData.password, 10)
-    res.json({"message": "Created User"})
-}
+export const createUser: RequestHandler = async (req, res) => {
+  const { name, lastName, email, password } = <RequestUserBody>req.body;
+  try {
+    let hashedPassword = await bcrypt.hash(password, 5)
+    await db.User.create({
+      name,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+    res.json({ message: "User successfully created" });
+  } catch (err: any) {
+    res.json(err.message);
+  }
+};
 
-export default createUser;
+export const getUsers: RequestHandler = async (_req, res) => {
+  try {
+    const users = await db.User.findAll();
+    res.json({ Users: users });
+    // res.json({"message": "working on it!"})
+  } catch (err: any) {
+    res.status(500).json({"message": "Not users Found"});
+  }
+};
--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,14 +1,11 @@
 import express from "express";
-import createUser from "../controllers/users-controller";
+import {getUsers, createUser }from "../controllers/users-controller";
 // const HttpError = require('../models/http-error')
 
 const router = express.Router();
 
-router.get("/", (_req, res) => {
-  console.log("GET users");
-  res.json({ message: "It works!" });
-});
+router.get("/", getUsers);
 
-router.post('/signup', createUser)
+router.post('/signup', createUser);
 
 module.exports = router;
